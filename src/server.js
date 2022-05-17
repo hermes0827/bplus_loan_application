dbeeb0456bd254189ed743791e4123ef25f89777
Nam Yeop Lee <hermes0827@gmail.com>
2022-05-17 11:44:59 +0900

    meta tag

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import favicon from "serve-favicon";
+import path from "path";
 import morgan from "morgan";
 import router from "./routes/router";
 import { createProxyMiddleware } from "http-proxy-middleware";
@@ -6,6 +8,7 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 const app = express();
 
 app.use(morgan("dev"));
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   "/api",
