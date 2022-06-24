ff98fe0befa24b74f458694a563254ce6bb1ae56
Nam Yeop Lee <hermes0827@gmail.com>
2022-06-25 01:10:20 +0900

    fixed sendNateon.js

--- src/services/sendNateon.js
+++ src/services/sendNateon.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const sendNateon = (name, phone_no) => {
-  const data = `신용정보 송부가 시도되었습니다.(성함 : ${name} / 연락처: ${phone_no}). 담당자께서는 확인을 부탁드립니다.`;
+  const data = `대출 신청이 접수되었습니다. (성함 : ${name} / 연락처: ${phone_no}). 담당자께서는 확인을 부탁드립니다.`;
 
   const options = {
     method: "POST",
