4a048534606b18f63dce242c4a4c93ac231828ad
Nam Yeop Lee <hermes0827@gmail.com>
2022-06-25 16:07:28 +0900

    fixed apply.js

--- src/public/js/apply.js
+++ src/public/js/apply.js
@@ -42,7 +42,7 @@ const fnSendData = async (sJsonText) => {
   );
   document.form.target = "authSend";
   document.form.action =
-    "https://www.creditinfo.co.kr:9004/nicecredit/auth/authSendGateway.cb#Intent;end";
+    "www.creditinfo.co.kr:9004/nicecredit/auth/authSendGateway.cb#Intent;scheme=https;package=com.android.chrome;end";
   await document.form.submit();
   window.location.href = "/kyc";
 };
