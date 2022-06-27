c5691df7dba9282ba81f3fbbeb4333965b1e47d7
Nam Yeop Lee <hermes0827@gmail.com>
2022-06-27 21:35:47 +0900

    fixed scrapingCertVAT.js

--- src/public/js/scraping/scrapingCertVAT.js
+++ src/public/js/scraping/scrapingCertVAT.js
@@ -17,7 +17,7 @@ const certVAT = async () => {
   };
 
   Date.prototype.BeforeOneYear = function () {
-    let mm = this.getMonth() + 1; // getMonth() is zero-based
+    let mm = this.getMonth() + 2; // getMonth() is zero-based
 
     return [this.getFullYear() - 1, (mm > 9 ? "" : "0") + mm].join("");
   };
