d59206fe40e68b8caf422e9d95a32245ab2d7c64
Nam Yeop Lee <hermes0827@gmail.com>
2022-07-22 15:54:30 +0900

    fixed controller.js

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -37,9 +37,9 @@ export const postKyc = (req, res) => {
 export const notAllowed = (req, res) => res.render("notAllowed");
 
 export const postCert = async (req, res) => {
-  // sendKakao("townloan_accepted", req.body.phone_no);
-  // sendNateon(req.body.name, req.body.phone_no);
-  // sendEmail(req.body.email);
+  sendKakao("townloan_accepted", req.body.phone_no);
+  sendNateon(req.body.name, req.body.phone_no);
+  sendEmail(req.body.email);
   res.render("cert");
 };
 
